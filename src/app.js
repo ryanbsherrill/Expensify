26ec29d4d7f649b816b87ea99a2b25ce13e423bd
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-09-25 00:13:37 -0400

    added react-router-dom, setup webpack for routing, setup base routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>Expensify</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <div>
+    From ExpenseDashboardPage
+  </div>
+);
+
+const CreateExpensePage = () => (
+  <div>
+    From CreateExpensePage
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    From EditExpensePage
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    From HelpPage
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={CreateExpensePage} exact={true}/>
+      <Route path="/edit" component={EditExpensePage} exact={true}/>
+      <Route path="/help" component={HelpPage} exact={true}/>
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
